7063954dd3f6f1dda85b39890bd7f2d08900627d
arashrai <arashrai17@gmail.com>
2021-04-18 16:20:30 -0700

    completed truth_booth

--- test_run_show.py
+++ test_run_show.py
@@ -14,5 +14,14 @@ girls = [
 
 show = AreYouTheOne(guys, girls)
 
-for k,v in show.name_to_id.items():
-    print(k, v)
+# for k,v in show.name_to_id.items():
+#     print(k, v)
+
+for p in show.all_possible_pairings:
+    print(p)
+
+
+show.truth_booth("Adam", "Amber", False)
+
+for p in show.all_possible_pairings:
+    print(p)
