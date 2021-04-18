820552547fbad03db02e8bcc38500856c999bb64
NatalieFleury <natalie.fleury99@gmail.com>
2021-04-18 15:20:52 -0700

    Added pairing to PossiblePairings class

--- PossiblePairing.py
+++ PossiblePairing.py
@@ -1,3 +1,17 @@
 class PossiblePairing:
+
+    guys = []
+    girls = []
+
+    # guys are the first tuple value in the pairs set 
+    pairs = set()
+
     def __init__(self, guys, girls):
-        pass
+        self.guys = guys
+        self.girls = girls
+        for ind, val in enumerate(guys):
+            self.pairs.add( (guys[ind], girls[ind]))
+        print(self.pairs)
+
+
+# PossiblePairing(["richy", "guy", "arash"], ["nat", "chelsea", "jess"])
